66e1dbaa7e5f13eb16723ec1087a5187936c8d2e
Octavian Santau <octavian.santau@palstor.ro>
2021-02-19 16:22:55 +0200

    Adaugare asociere device cu user pt. live update.

--- schema/add_wenglor_device.sql
+++ schema/add_wenglor_device.sql
@@ -1,3 +1,3 @@
 --adaugare device care reprezinta camera wenglor
 INSERT INTO public.tc_devices( name, uniqueid) VALUES ('wenglor_cam','wenglor_cam') ON CONFLICT DO NOTHING;
-
+update public.tc_user_device set deviceid = (select id from tc_devices where uniqueid = 'wenglor_cam') WHERE userid = 1;
